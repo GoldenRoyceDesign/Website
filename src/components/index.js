7d661b5abe435406846a85d6ea32d841710c6b10
premnathdevpic <premnathpichaimuthu67@gmail.com>
2024-01-13 20:49:42 +0530

    grd creation responsive

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import About from "./About";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  About,
+  CardDeal,
+  Business,
+  CTA,
+  Footer,
+  Testimonials,
+  Hero,
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff5350",
+        secondary: "#fff349",
+        dimWhite: "rgb(255, 255, 255)",
+        dimBlue: "rgb(9, 151, 124)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
